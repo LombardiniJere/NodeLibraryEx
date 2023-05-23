2ab37d9ec0180a3880591779fa233802fedbf12b
LombardiniJere <jere.lombardini@gmail.com>
2023-05-23 20:37:26 -0300

    providerssetup

--- src/providers/bookProvider.js
+++ src/providers/bookProvider.js
@@ -1,9 +1,9 @@
 const { Op } = require("sequelize");
-const { Book } = require("../models");
+const { bookModel } = require("../models");
 
 const createBook = async (bookOptions) => {
   try {
-    const newBook = await Book.create(bookOptions);
+    const newBook = await bookModel.create(bookOptions);
     // const ticket = await Ticket.create({ used: false, BookIsbn: newBook.isbn });
     return newBook;
   } catch (error) {
@@ -13,7 +13,7 @@ const createBook = async (bookOptions) => {
 
 const getBook = async (isbn) => {
   try {
-    const book = await Book.findByPk(isbn, { include: [{ all: true }] });
+    const book = await bookModel.findByPk(isbn, { include: [{ all: true }] });
     if (book) {
       return book;
     } else {
@@ -30,7 +30,7 @@ const getBooks = async (criteria) => {
     if (criteria) {
       options = { ...options, where: { [Op.or]: criteria } };
     }
-    const Books = await Book.findAll(options);
+    const Books = await bookModel.findAll(options);
 
     if (Books) {
       return Books;
@@ -44,47 +44,44 @@ const getBooks = async (criteria) => {
   }
 };
 
-const updateBook = async (bookIsbn, bookOptions) => {
-  try {
-    await getBook(bookIsbn);
-    const [numRowsUpdated] = await Book.update(bookOptions, {
-      where: { isbn: bookIsbn },
-    });
-    console.log(`Se actualizaron ${numRowsUpdated} filas en la DB`);
-    return Book.findByPk(bookIsbn);
-  } catch (error) {
-    throw error;
-  }
-};
+// const updateBook = async (bookIsbn, bookOptions) => {
+//   try {
+//     await getBook(bookIsbn);
+//     const [numRowsUpdated] = await bookModel.update(bookOptions, {
+//       where: { isbn: bookIsbn },
+//     });
+//     console.log(`Se actualizaron ${numRowsUpdated} filas en la DB`);
+//     return bookModel.findByPk(bookIsbn);
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-const deleteBook = async (bookIsbn) => {
-  try {
-    return Book.destroy({ where: { isbn: bookIsbn } });
-  } catch (error) {
-    throw error;
-  }
-};
+// const deleteBook = async (bookIsbn) => {
+//   try {
+//     return bookModel.destroy({ where: { isbn: bookIsbn } });
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-const validateBook = async (isbn) => {
-  try {
-    const book = await Book.findOne({
-      where: { isbn },
-    });
-    if (book) {
-      return book;
-    } else {
-      return false;
-    }
-  } catch (error) {
-    throw error;
-  }
-};
+// const validateBook = async (isbn) => {
+//   try {
+//     const book = await bookModel.findOne({
+//       where: { isbn },
+//     });
+//     if (book) {
+//       return book;
+//     } else {
+//       return false;
+//     }
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
 module.exports = {
   createBook,
   getBook,
   getBooks,
-  updateBook,
-  deleteBook,
-  validateBook,
 };
