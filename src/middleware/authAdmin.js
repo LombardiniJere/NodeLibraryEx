4e181626c1f86a9496cd24707e62dc2cf39231bc
LombardiniJere <jere.lombardini@gmail.com>
2023-05-31 11:05:49 -0300

    .

--- src/middleware/authAdmin.js
+++ src/middleware/authAdmin.js
@@ -2,8 +2,8 @@ const passport = require("passport");
 const authCheck = require("./authCheck");
 // authIsAdmin able to create-delete user
 const authIsAdmin = (req, res, next) => {
-  console.log(req.user);
-  if (authCheck && req.user.role === "ADMIN") {
+  
+  if (req.isAuthenticated() && req.user.role === "ADMIN") {
     return next();
   }
   res.status(401).json({ error: "Not authorized" });
